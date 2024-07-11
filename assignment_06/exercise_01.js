679381a64ea80bfc553e3dc26d7b320fc30c73e6
Mac <mac@MacBook-Pro-2.local>
2024-07-11 16:10:07 +0700

    add ass 06

--- assignment_06/exercise_01.js
+++ assignment_06/exercise_01.js
@@ -2,41 +2,30 @@ const data = require('./mockData')
 
 function getUserInforById(users, userId, callback) {
     setTimeout(() => {
-        const user = users.find((item, index) => {
-            if (item.id === userId) {
-                return item;
-            }
-        })
+        const user = users.find(item=>item.id === userId)
         if (user) {
-            callback(null, user);
+            callback(user);
         } else {
             console.warn("User not found");
             new Error("User not found")
         }
     }, 2000)
 }
+getUserInforById(data, 1, r => console.log(r))
 
 function userCapitalize(users, callback) {
     setTimeout(() => {
-        const userCapitalize = {...users , name:users.name.toUpperCase()}
-        callback(null,userCapitalize)
+        const userCapitalize = { ...users, name: users.name.toUpperCase() }
+        callback(userCapitalize)
     }, 1500)
 
 }
-getUserInforById(data,1,(err,result)=>{
-    if(err){
-        console.error("Error!" + err)
-        return;
-    }else{
-        userCapitalize(result , (err,result)=>{
-            if(err){
-                console.error(err)
-                return;
-            }else{
-                console.log("Procesed data users", 
-                    result
-                );
-            }
-        })
-    }
+getUserInforById(data, 1, (result) => {
+    userCapitalize(result, (result) => {
+
+        console.log("Procesed data users",
+            result
+        );
+
+    })
 })
---
+++ assignment_06/exercise_03.js
@@ -0,0 +1,10 @@
+const { readFileContent } = require("../assigment_3/ex_3")
+const { writeFile } = require("../assigment_3/ex_4")
+const filePath = './file.txt';
+const promise = async () =>{
+        await readFileContent(filePath ,async(e,r)=>{
+            await writeFile('./input.txt', r + ' hiiii' , (e,r)=>r)
+        })
+}
+promise().then(res=>console.log(res)).catch(er=>console.log(er))
+
